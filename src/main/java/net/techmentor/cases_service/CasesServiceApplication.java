b16038b04d741b3ed116016d1ad93458ec8e9d1a
Muhammad A <muhammadalyadam@gmail.com>
2025-05-15 10:11:44 +0300

    build: Add project base

---
+++ src/main/java/net/techmentor/cases_service/CasesServiceApplication.java
@@ -0,0 +1,13 @@
+package net.techmentor.cases_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasesServiceApplication.class, args);
+	}
+
+}
